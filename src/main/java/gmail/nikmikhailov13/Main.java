62def620d443f4c565bee30eea119f9d167f3120
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-02-20 18:26:34 +0100

    add hello world

--- src/main/java/gmail/nikmikhailov13/Main.java
+++ src/main/java/gmail/nikmikhailov13/Main.java
@@ -2,6 +2,6 @@ package gmail.nikmikhailov13;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Nikita!");
+        System.out.println("Hello World!");
     }
 }
